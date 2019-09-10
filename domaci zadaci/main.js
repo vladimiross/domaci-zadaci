4bcdb8ae4d5909f88459a05b76b3370c23401ea4
Vladimiros <vladosarapa@gmail.com>
2019-09-10 16:51:14 +0200

    Add files via upload

---
+++ domaci zadaci/main.js
@@ -0,0 +1,138 @@
+//prvi
+
+// var unos = Number(prompt("unesite bilo koji broj"));
+// function count(){
+//     var sum = 0;
+//     for(i = 1; i < unos; i++){
+//        sum = sum + i;
+//     }
+//      alert(sum)
+// }
+ 
+//drugi
+
+// var unos = Number(prompt("unesite bilo koji broj"));
+// function count(){
+//     var sum = 1;
+//     for(i = 1; i < unos; i++){
+//        sum = sum * i;
+//     }
+//      alert(sum)
+//      }
+
+//treci
+
+// var unos = Number(prompt("unesite bilo koji broj"));
+// function count(){
+//     var sum = 0;
+    
+//     for(i = 1; i < unos; i+=2){
+//         sum = sum + i;
+//     }
+    
+//     alert(sum);
+// }
+
+//cetvrti
+// ili while(unos > 0){}
+// var unos = Number(prompt("unesite bilo koji broj"));
+// function count(){
+//  var zbir = 0;
+// while(unos){
+//   zbir += unos % 10;
+//   unos = Math.floor(unos / 10)
+ 
+// }
+// alert("suma cifara unesenog broja je "+ zbir);
+// };
+ //peti
+
+//  var unos = Number(prompt("unesite bilo koji broj"));
+//  function count(){
+//    var max = 0;
+//    var broj;
+//    while(unos){
+//      broj = unos % 10;
+//      unos = Math.floor(unos / 10);
+//      max = max > broj ? max : broj;
+//      }
+//      alert("najveci broj je "+ max)
+//    }
+ 
+  //sesti
+
+// function provjeri(){
+//     var ina = document.getElementById("opa");
+//     if(ina.value == "5"){
+//         ina.style.border= "1px solid red";
+//     }
+// }
+//sedmi
+
+// function izracunaj(){
+//     var prvi= Number(document.getElementById("prvi").value);
+//     var drugi= Number(document.getElementById("drugi").value);
+//     alert(prvi + drugi);
+// }
+
+// function povecaj(){
+//     var p = document.getElementById("p").innerHTML;
+//     alert(p);
+// }
+
+// var a = prompt("unesite neki tekst");
+// alert("unijeli ste "+ a)
+
+// function pro(x){
+//     if(x instanceof Array){
+//         alert(x.length)
+//     }
+// }
+// pro([1, 2, 3, 4])
+
+// function allah(){
+//     alert("fokus je izgubljen");
+// }
+
+// var a= prompt("unesi neki tekst");
+// document.getElementById("moj").innerHTML=a;
+
+// function saberi(){
+//     var x = Number(document.getElementById("x").value);
+//     var y = Number(document.getElementById("y").value);
+    
+//      document.getElementById("rezultat").value= x+y;
+// }
+// function oduzmi(){
+//     var x = Number(document.getElementById("x").value);
+//     var y = Number(document.getElementById("y").value);
+    
+//      document.getElementById("rezultat").value= x-y;
+// }
+// function pomnozi(){
+//     var x = Number(document.getElementById("x").value);
+//     var y = Number(document.getElementById("y").value);
+    
+//      document.getElementById("rezultat").value= x*y;
+// }
+// function podijeli(){
+//     var x = Number(document.getElementById("x").value);
+//     var y = Number(document.getElementById("y").value);
+    
+//      document.getElementById("rezultat").value= x/y;
+// }
+
+// var unos = Number(prompt("unesi broj"));
+// for(i = 0; i < unos; i++){
+//     if(unos%i==0){
+//         alert(i)
+//     }
+// }
+
+// function ja(x, y){
+//     for(i =0; i < y; i++){
+//     console.log(x)
+//     }
+      
+//     }
+//     ja("popi kavu", 3);
